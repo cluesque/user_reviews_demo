93a1c46dc09e5added9e2322e6182e74c97d0705
Bill Kirtley <bill@spiffy.com>
2011-05-26 19:29:49 -0400

    Test that the database will enforce uniqueness

--- test/unit/user_review_test.rb
+++ test/unit/user_review_test.rb
@@ -8,13 +8,28 @@ class UserReviewTest < ActiveSupport::TestCase
   end
   def test_user_review_activerecord_check
     
+    book_review = UserReview.new(:user => @user, :book => @book)
+    assert book_review.save
+
+    movie_review = UserReview.new(:user => @user, :movie => @movie)
+    assert movie_review.save
+
+    duplicate_review = UserReview.new(:user => @user, :movie => @movie)
+    assert !duplicate_review.save # No, cannot save that
+  end
+
+  def test_user_review_database_check
     book_review = UserReview.create(:user => @user, :book => @book)
     assert book_review.save
 
     movie_review = UserReview.create(:user => @user, :movie => @movie)
     assert movie_review.save
-    
-    duplicate_review = UserReview.create(:user => @user, :movie => @movie)
-    assert !duplicate_review.save # No, cannot save that
+
+    assert_raise ActiveRecord::RecordNotUnique do
+      UserReview.connection.execute <<-EOSQL
+      INSERT INTO user_reviews (user_id, book_id)
+        VALUES (#{@user.id}, #{@book.id})
+      EOSQL
+    end
   end
 end
