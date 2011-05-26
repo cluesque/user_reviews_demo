131d5afd2dc0cff3d9cb8376aa628f5695ae2f4c
Bill Kirtley <bill@spiffy.com>
2011-05-26 18:04:15 -0400

    Stock rails generated framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserReviewsDemo::Application.config.secret_token = 'aeee3d0170a67d5482140e45c84fe6562e235214a661279a494a3d7377137f8f63538bacf2c18261517ed4e74d921dec042831d0a1642f20b6370dc49ee7f6f9'
