52ea289f163ab168efa2db30e0c4df87a3b58c12
Bill Kirtley <bill@spiffy.com>
2011-05-27 14:50:41 -0400

    Nicer foreign keys with foreigner

--- db/migrate/20110526155535_create_user_reviews.rb
+++ db/migrate/20110526155535_create_user_reviews.rb
@@ -3,18 +3,14 @@ class CreateUserReviews < ActiveRecord::Migration
     create_table :user_reviews
     change_table :user_reviews do |t|
       t.references :user, :null => false
+      t.foreign_key :users
       t.references :movie
+      t.foreign_key :movies
       t.references :book
+      t.foreign_key :books
     end
     # ActiveRecord handy unique constraint syntax sugar:
     add_index :user_reviews, [:user_id, :book_id, :movie_id], :unique => true
-    # For bonus points, use the foreigner gem to pretty these up:
-    execute "ALTER TABLE user_reviews ADD CONSTRAINT user_reviews_user_id_fk
-             FOREIGN KEY (user_id) REFERENCES books(id)"
-    execute "ALTER TABLE user_reviews ADD CONSTRAINT user_reviews_book_id_fk
-             FOREIGN KEY (book_id) REFERENCES books(id)"
-    execute "ALTER TABLE user_reviews ADD CONSTRAINT user_reviews_movie_id_fk
-             FOREIGN KEY (movie_id) REFERENCES movies(id)"
   end
 
   def self.down
